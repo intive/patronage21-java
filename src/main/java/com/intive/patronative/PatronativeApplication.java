f7d60b1edbf71f95bbd3bf93af2cde646c8e85cd
Karolina <kwicynska@gmail.com>
2021-03-19 21:40:26 +0100

    IP2-83 - initialize springboot

---
+++ src/main/java/com/intive/patronative/PatronativeApplication.java
@@ -0,0 +1,13 @@
+package com.intive.patronative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronativeApplication.class, args);
+	}
+
+}
