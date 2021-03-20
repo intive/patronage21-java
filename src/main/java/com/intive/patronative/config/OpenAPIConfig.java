654dc00457dd90fba42998453ef4317dd166dac6
lzolkiewski <lukaszolkiewski@gmail.com>
2021-03-20 14:24:46 +0100

    IP2-92 - add OpenAPI

---
+++ src/main/java/com/intive/patronative/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.intive.patronative.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI patronativeOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Patronative API")
+                .description("Users module")
+                .version("v0.0.1"));
+
+    }
+    
+}
