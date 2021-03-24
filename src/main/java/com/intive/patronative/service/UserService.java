d7fb87c3789fa418f44edc7bc2fe72efc80cfe2d
barbara-tyndorf <barbaratyndorf@gmail.com>
2021-03-24 12:09:00 +0100

    change method name to update

--- src/main/java/com/intive/patronative/service/UserService.java
+++ src/main/java/com/intive/patronative/service/UserService.java
@@ -2,20 +2,17 @@ package com.intive.patronative.service;
 
 import com.intive.patronative.model.User;
 import com.intive.patronative.model.UserDTO;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@NoArgsConstructor
 public class UserService {
 
-    public UserService() {
-
-    }
-
-    public void updateUser(UserDTO userDTO) {
+    public void update(UserDTO userDTO) {
         log.info("Evoking service...");
     }
-
 
 }
