8fc40eeaee9e976b9cff37a6a03fe5e07d1e59e1
barbara-tyndorf <barbaratyndorf@gmail.com>
2021-03-26 17:25:46 +0100

    add findBy method using queryParam

---
+++ src/main/java/com/intive/patronative/service/UserService.java
@@ -0,0 +1,24 @@
+package com.intive.patronative.service;
+
+import com.intive.patronative.model.User;
+import com.intive.patronative.dto.UserDTO;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Slf4j
+@Service
+@NoArgsConstructor
+public class UserService {
+
+    public void update(UserDTO userDTO) {
+        log.info("Evoking service to update user data...");
+    }
+
+    public void findBy(Map<String, String> params){
+        log.info("Evoking service to find users...");
+    }
+
+}
