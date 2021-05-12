ff3d96766f72b6eab7770fb04e925955f043701c
tomasz.moryl <tomasz.moryl@intive.com>
2021-05-12 22:12:38 +0200

    AAA działa

--- src/test/java/com/intive/patronative/PatronativeApplicationTests.java
+++ src/test/java/com/intive/patronative/PatronativeApplicationTests.java
@@ -1,7 +1,13 @@
 package com.intive.patronative;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PatronativeApplicationTests {
+
+    @Test
+    public void asd() {
+
+    }
 }
