80bb046c31d7121c5e459cca22425c1eaffbfe18
tomasz.moryl <tomasz.moryl@intive.com>
2021-05-12 22:19:14 +0200

    AAA działa

--- src/test/java/com/intive/patronative/PatronativeApplicationTests.java
+++ src/test/java/com/intive/patronative/PatronativeApplicationTests.java
@@ -1,13 +1,23 @@
 package com.intive.patronative;
 
+import com.intive.patronative.repository.RoleRepository;
+import com.intive.patronative.repository.model.Role;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @SpringBootTest
 class PatronativeApplicationTests {
 
+    @Autowired
+    RoleRepository roleRepository;
+
     @Test
     public void asd() {
-
+        final Optional<Role> role = roleRepository.findById(BigDecimal.ONE);
+        role.ifPresent(System.out::println);
     }
 }
