52e191088149d1faacd97812c97d77d3d16914dc
lzolkiewski <62542051+lzolkiewski@users.noreply.github.com>
2021-05-13 18:43:39 +0200

    IP2-525 - create projects' endpoint (#45)

--- src/main/java/com/intive/patronative/controller/advice/UserControllerAdvice.java
+++ src/main/java/com/intive/patronative/controller/advice/UserControllerAdvice.java
@@ -1,6 +1,9 @@
 package com.intive.patronative.controller.advice;
 
 import com.intive.patronative.exception.InvalidArgumentException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,14 +30,23 @@ public class UserControllerAdvice {
 
     @ResponseBody
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ValidationErrorResponse methodArgumentTypeMismatchHandler(final MethodArgumentTypeMismatchException exception) {
+        return buildErrorResponse(new ViolationError(exception.getName(), exception.getValue(), exception.getCause().getMessage()));
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(InvalidArgumentException.class)
     public ValidationErrorResponse invalidArgumentHandler(final InvalidArgumentException exception) {
         return buildErrorResponse(exception.getFieldErrors());
     }
 
-    @Value
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
     private class ValidationErrorResponse {
-        List<ViolationError> violationErrors;
+        private List<ViolationError> violationErrors;
     }
 
     @Value
@@ -43,8 +57,14 @@ public class UserControllerAdvice {
     }
 
     private ValidationErrorResponse buildErrorResponse(final List<FieldError> fieldErrors) {
-        return new ValidationErrorResponse(fieldErrors.stream().map(fieldError -> new ViolationError(fieldError.getField(),
+        return (fieldErrors == null)
+                ? new ValidationErrorResponse()
+                : new ValidationErrorResponse(fieldErrors.stream().map(fieldError -> new ViolationError(fieldError.getField(),
                 fieldError.getRejectedValue(), fieldError.getDefaultMessage())).collect(Collectors.toList()));
+    }
+
+    private ValidationErrorResponse buildErrorResponse(final ViolationError violationError) {
+        return new ValidationErrorResponse(Collections.singletonList(violationError));
     }
 
 }
---
+++ src/main/java/com/intive/patronative/dto/ProjectsResponseDTO.java
@@ -0,0 +1,8 @@
+package com.intive.patronative.dto;
+
+import lombok.Value;
+
+@Value
+public class ProjectsResponseDTO {
+    Object projects;
+}
