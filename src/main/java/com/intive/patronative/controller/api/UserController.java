44afa9f19003f72f5b68cd252725d643fd4cba22
lzolkiewski <62542051+lzolkiewski@users.noreply.github.com>
2021-05-19 10:16:26 +0200

    IP2-231 - OpenAPI documentation update (#50)

--- src/main/java/com/intive/patronative/controller/api/UserController.java
+++ src/main/java/com/intive/patronative/controller/api/UserController.java
@@ -4,6 +4,9 @@ import com.intive.patronative.dto.UserEditDTO;
 import com.intive.patronative.dto.UserResponseDTO;
 import com.intive.patronative.dto.model.UserDTO;
 import com.intive.patronative.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,17 +28,33 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/{login}")
+    @Operation(summary = "Fetch user by login")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Fetch successful"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<UserResponseDTO> getUser(@PathVariable(name = "login") final String login) {
         return ResponseEntity.ok(userService.getUserByLogin(login));
     }
 
     @PostMapping
+    @Operation(summary = "Create user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Creation successful"),
+            @ApiResponse(responseCode = "400", description = "Invalid data passed"),
+    })
     public ResponseEntity<String> saveUser(@Valid @RequestBody final UserDTO userDTO) {
         userService.saveUser(userDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body("User saved");
     }
 
     @PutMapping
+    @Operation(summary = "Update user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Update successful"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "422", description = "Invalid data passed")
+    })
     public ResponseEntity<Void> update(@RequestBody final UserEditDTO userEditDTO) {
         userService.updateUser(userEditDTO);
         return ResponseEntity.ok().build();
--- src/main/java/com/intive/patronative/controller/frontend/GroupFrontController.java
+++ src/main/java/com/intive/patronative/controller/frontend/GroupFrontController.java
@@ -2,6 +2,8 @@ package com.intive.patronative.controller.frontend;
 
 import com.intive.patronative.dto.model.GroupsDTO;
 import com.intive.patronative.service.GroupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ public class GroupFrontController {
     private final GroupService groupService;
 
     @GetMapping
+    @Operation(summary = "Fetch groups")
+    @ApiResponse(responseCode = "200", description = "Fetch successful")
     public ResponseEntity<GroupsDTO> getGroups() {
         return ResponseEntity.ok().body(groupService.getGroups());
     }
